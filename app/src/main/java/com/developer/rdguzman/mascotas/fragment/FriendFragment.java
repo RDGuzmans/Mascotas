9d92ef57642828167ad930a6d5fc6df61c1e2459
RDGuzmans <dguzman.sant@gmail.com>
2017-05-11 11:29:49 -0500

    Initial commit

---
+++ app/src/main/java/com/developer/rdguzman/mascotas/fragment/FriendFragment.java
@@ -0,0 +1,26 @@
+package com.developer.rdguzman.mascotas.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.developer.rdguzman.mascotas.R;
+
+/**
+ * Created by Guzman on 01/05/2017.
+ */
+
+public class FriendFragment extends Fragment {
+    public FriendFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friend, container, false);
+    }
+}
